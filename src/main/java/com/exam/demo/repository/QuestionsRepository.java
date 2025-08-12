ccca8a9b83b825fb2ee35d60bfe697498d2fbfb2
pari <parivashcheraghi40@gmail.com>
2025-08-12 13:58:52 +0330

    feat(Questions):add Question repository  | parivash cheraghi

---
+++ src/main/java/com/exam/demo/repository/QuestionsRepository.java
@@ -0,0 +1,13 @@
+package com.exam.demo.repository;
+
+import com.exam.demo.model.Questions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionsRepository extends JpaRepository<Questions, Integer> {
+    List<Questions> findByExamId(Integer examId);
+
+}
