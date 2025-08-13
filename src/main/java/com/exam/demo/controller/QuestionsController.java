d26b6a2c9f13f18e7008aee67acd9383f3ce4866
pari <parivashcheraghi40@gmail.com>
2025-08-13 11:22:00 +0330

    refactor(Questions):add edit questions to controller | parivash cheraghi

--- src/main/java/com/exam/demo/controller/QuestionsController.java
+++ src/main/java/com/exam/demo/controller/QuestionsController.java
@@ -3,6 +3,7 @@ package com.exam.demo.controller;
 import com.exam.demo.dto.DescriptiveQuestionDto;
 import com.exam.demo.dto.MultipleChoiceQuestionDto;
 import com.exam.demo.dto.QuestionsDto;
+import com.exam.demo.dto.UpdateQuestionTextDto;
 import com.exam.demo.model.Questions;
 import com.exam.demo.service.QuestionsService;
 import jakarta.validation.Valid;
@@ -32,9 +33,40 @@ public class QuestionsController {
         Questions question = questionService.addDescriptiveQuestion(dto);
         return new ResponseEntity<>(question, HttpStatus.CREATED);
     }
+
     @GetMapping("/exams/{examId}")
-    public ResponseEntity<List<QuestionsDto>> getQuestionsByExam(@PathVariable Integer examId) {
+    public ResponseEntity<List<QuestionsDto>> getQuestionsByExamId(@PathVariable Integer examId) {
         List<QuestionsDto> questions = questionService.getQuestionsByExamId(examId);
+        return ResponseEntity.ok(questions);
+    }
+
+    @PutMapping("/{id}/QuestionText")
+    public ResponseEntity<Questions> updateQuestionText(
+            @PathVariable Integer id,
+            @RequestBody UpdateQuestionTextDto dto) {
+
+        Questions updated = questionService.updateQuestionText(id, dto.getNewQuestionText());
+        return ResponseEntity.ok(updated);
+    }
+
+    @PutMapping("/{id}/multiple-choice")
+    public ResponseEntity<Questions> updateMultipleChoiceQuestion(
+            @PathVariable Integer id,
+            @Valid @RequestBody MultipleChoiceQuestionDto dto) {
+
+        Questions updated = questionService.updateMultipleChoiceQuestion(id, dto);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteQuestion(@PathVariable Integer id) {
+        questionService.deleteQuestionById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/exams/{examId}")
+    public ResponseEntity<List<QuestionsDto>> DeleteAllQuestionsByExamId(@PathVariable Integer examId) {
+        List<QuestionsDto> questions = questionService.deleteAllQuestionsByExamId(examId);
         return ResponseEntity.ok(questions);
     }
 
