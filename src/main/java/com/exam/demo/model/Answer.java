5b63848a1fe6f5a21fa72962de5e88e7ea4a5fb0
pari <parivashcheraghi40@gmail.com>
2025-08-17 12:12:15 +0330

    feat (Answer):add Answer entity | parivash cheraghi

---
+++ src/main/java/com/exam/demo/model/Answer.java
@@ -0,0 +1,29 @@
+package com.exam.demo.model;
+
+import com.exam.demo.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "answers")
+public abstract class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Questions question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User student;
+
+    private Double score;
+
+    private boolean corrected = false;
+}
