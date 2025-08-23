2e8b3dd88abf8cebab7a5c60c652beaba0274c91
pari <parivashcheraghi40@gmail.com>
2025-08-23 10:07:02 +0330

    refactor(login): config for login | parivash cheraghi

---
+++ src/main/java/com/exam/demo/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package com.exam.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/exams");
+    }
+}
