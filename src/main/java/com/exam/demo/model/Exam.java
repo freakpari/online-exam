04c84536eebeccaf6c48a7216f7d187a4c421d77
pari <parivashcheraghi40@gmail.com>
2025-08-23 10:06:01 +0330

    refactor(exam):annotations replaced | parivash cheraghi

--- src/main/java/com/exam/demo/model/Exam.java
+++ src/main/java/com/exam/demo/model/Exam.java
@@ -4,14 +4,15 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "exams")
 public class Exam {
